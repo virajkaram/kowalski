91f0be58afcd92c51df6ef7871d8b75bf29815e2
viraj21197 <karambelkarvraj21197@gmail.com>
2025-08-14 18:11:29 -0400

    update fp hist

--- kowalski/ingesters/ingest_wtp_alerts.py
+++ kowalski/ingesters/ingest_wtp_alerts.py
@@ -453,6 +453,171 @@ def process_file(argument_list: Sequence):
 
         return fp_hists
 
+    def update_fp_hists(alert, formatted_fp_hists):
+        # update the existing fp_hist with the new one
+        # instead of treating it as a set,
+        # if some entries have the same jd, keep the one with the highest alert_mag
+
+        # if we have no fp_hists to add, we don't do anything
+        if len(formatted_fp_hists) == 0:
+            return
+
+        with timer(
+            f"Updating fp_hists of {alert['objectId']} {alert['candid']}",
+            verbose > 1,
+        ):
+            # pipeline that returns the very last fp_hists entry from the DB
+            last_fp_hist_pipeline = [
+                # 0. match the document and check that the fp_hists field exists
+                {"$match": {"_id": alert["objectId"], "fp_hists": {"$exists": True}}},
+                # 1. add a field which is the size of the fp_hists array
+                {"$addFields": {"n_fp_hists": {"$size": "$fp_hists"}}},
+                # 2. only keep the last fp_hists entry and call it fp_hist
+                {
+                    "$project": {
+                        "fp_hist": {"$arrayElemAt": ["$fp_hists", -1]},
+                        "n_fp_hists": 1,
+                    }
+                },
+                # 3. project only the jd and alert_mag, alert_ra, alert_dec fields in the fp_hists, as well as the n_fp_hists
+                {
+                    "$project": {
+                        "fp_hist": {
+                            "jd": "$fp_hist.jd",
+                            "alert_mag": "$fp_hist.alert_mag",
+                            "alert_ra": "$fp_hist.alert_ra",
+                            "alert_dec": "$fp_hist.alert_dec",
+                        },
+                        "n_fp_hists": 1,
+                    }
+                },
+            ]
+
+            # pipeline that updates the fp_hists array if necessary
+            update_pipeline = [
+                # 0. match the document
+                {"$match": {"_id": alert["objectId"]}},
+                # 1. concat the new fp_hists with the existing ones
+                {
+                    "$project": {
+                        "all_fp_hists": {
+                            "$concatArrays": [
+                                {"$ifNull": ["$fp_hists", []]},
+                                formatted_fp_hists,
+                            ]
+                        }
+                    }
+                },
+                # 2. unwind the resulting array to get one document per fp_hist
+                {"$unwind": "$all_fp_hists"},
+                # 3. group by jd and keep the one with the highest alert_mag for each jd
+                {
+                    "$set": {
+                        "all_fp_hists.alert_mag": {
+                            "$cond": {
+                                "if": {
+                                    "$eq": [
+                                        {"$type": "$all_fp_hists.alert_mag"},
+                                        "missing",
+                                    ]
+                                },
+                                "then": -99999.0,
+                                "else": "$all_fp_hists.alert_mag",
+                            }
+                        }
+                    }
+                },
+                # 4. sort by jd and alert_mag
+                {
+                    "$sort": {
+                        "all_fp_hists.jd": 1,
+                        "all_fp_hists.alert_mag": 1,
+                    }
+                },
+                # 5. group all the deduplicated fp_hists back into an array, keeping the first one (the brightest at each jd)
+                {
+                    "$group": {
+                        "_id": "$all_fp_hists.jd",
+                        "fp_hist": {"$first": "$$ROOT.all_fp_hists"},
+                    }
+                },
+                # 6. sort by jd again
+                {"$sort": {"fp_hist.jd": 1}},
+                # 7. group all the fp_hists documents back into a single array
+                {"$group": {"_id": None, "fp_hists": {"$push": "$fp_hist"}}},
+                # 8. project only the new fp_hists array
+                {"$project": {"fp_hists": 1, "_id": 0}},
+            ]
+
+            n_retries = 0
+            while True:
+                try:
+                    # run the update pipeline
+                    new_fp_hists = (
+                        mongo.db[collection_alerts_aux]
+                        .aggregate(
+                            update_pipeline,
+                            allowDiskUse=True,
+                        )
+                        .next()
+                        .get("fp_hists", [])
+                    )
+
+                    # we apply some conditions when running find_one_and_update to avoid concurrency
+                    # issues where another process might have updated the fp_hists while we were
+                    # calculating our updated fp_hists
+
+                    update_conditions = {
+                        "_id": alert["objectId"],
+                    }
+
+                    result = mongo.db[
+                        collection_alerts_aux
+                    ].find_one_and_update(
+                        update_conditions,
+                        {"$set": {"fp_hists": new_fp_hists}},
+                    )
+                except Exception as e:
+                    log(
+                        f"Error occured trying to update fp_hists of {alert['objectId']} {alert['candid']}: {str(e)}"
+                    )
+                    result = None
+                if (
+                    result is None
+                ):  # conditions not met, likely to be a concurrency issue, retry
+                    n_retries += 1
+                    if n_retries > 10:
+                        log(
+                            f"Failed to update fp_hists of {alert['objectId']} {alert['candid']}"
+                        )
+                        break
+                    else:
+                        log(
+                            f"Retrying to update fp_hists of {alert['objectId']} {alert['candid']}"
+                        )
+                        # add a random sleep between 0 and 5s, this should help avoid multiple processes from retrying at the exact same time
+                        time.sleep(np.random.uniform(0, 5))
+                else:
+                    break
+
+            # query the DB for the last 30 days of fp_hists to get the updated fp_hists
+            new_fp_hists = list(
+                mongo.db[collection_alerts_aux]
+                .find(
+                    {
+                        "_id": alert["objectId"],
+                    },
+                    {"fp_hists": 1},
+                )
+                .sort([("jd", 1)])
+            )
+            if len(new_fp_hists) > 0:
+                new_fp_hists = new_fp_hists[0]["fp_hists"]
+            else:
+                new_fp_hists = []
+
+            return new_fp_hists
+
 
     def process_alert(alert: Mapping, topic: str, cross_match_config: dict):
         """Alert brokering task run by dask.distributed workers
@@ -562,7 +727,7 @@ def process_file(argument_list: Sequence):
                     upsert=True,
                 )
 
-            ## TODO : Figure out whether we need to update forced photometry for every candidate
+                fp_hists = update_fp_hists(alert, fp_hists)
 
             # Crossmatch exisiting alert with most recent record in ZTF_alerts and update aux
             # with timer(
